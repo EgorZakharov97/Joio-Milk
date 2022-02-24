e15c1f5ff209283df97040ba590949c18a499eba
egorzakharov97 <egor.zakharov97@gmail.com>
2022-02-23 21:18:06 -0600

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# git pull > update-logs/pull.log &
-# npm i > update-logs/install.log &
-# npm run build > update-logs/build.log &
-# pm2 restart joio > update-logs/restart.log
-
-pwd
+git pull &
+npm i &
+npm run build &
+pm2 restart joio
